ae193579fd723a1c148e5ee858f0e1a42f947d31
philippeawono <philippeawono@gmail.com>
2023-08-10 18:31:43 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A progam that prints a line to the standard
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdoud);
+	return (1);
+}
+
